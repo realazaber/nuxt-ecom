d7b751655ababb69c3c587333fcc2ab9399b1c3b
realazaber <azcooldude8@gmail.com>
2023-07-12 20:41:01 +0200

    Desktop tablet nav

---
+++ ecom/components/universal/nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav
+    class="fixed top-0 bg-pri text-white left-0 right-0 h-12 flex flex-row justify-between items-center px-4"
+  >
+    <nuxt-link to="/" class="font-bold">Fashion Fusion</nuxt-link>
+    <div class="flex flex-row justify-center items-center gap-3">
+      <nuxt-link to="/products">Products</nuxt-link>
+      <nuxt-link to="/about">About us</nuxt-link>
+      <nuxt-link to="/contact">Contact</nuxt-link>
+    </div>
+  </nav>
+</template>
